7a0111c070c6b2a95414f97288c4c478fead16a7
caromd <rcdeppe@yahoo.co.uk>
2016-04-26 10:02:55 +0000

    Added uppercase for document & item codes

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,14 @@
 class Document < ActiveRecord::Base
   validates :code, presence: true, uniqueness: true
+  before_validation :uppercase_code
   belongs_to :user
   belongs_to :location
   validates :location, presence: true
   validates :user, presence: true
   has_many :lines, :dependent => :restrict_with_error
   accepts_nested_attributes_for :lines, reject_if: :all_blank, allow_destroy: true
+
+  def uppercase_code
+    code.upcase!
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ActiveRecord::Base
     validates :code, presence: true, uniqueness: true
+    before_validation :uppercase_code
     validates :description, presence: true
     has_many :lines, :dependent => :restrict_with_error   
     def code_and_description
+        
         "#{code}: #{description}"
+    end
+    def uppercase_code
+        code.upcase!
     end
 end
